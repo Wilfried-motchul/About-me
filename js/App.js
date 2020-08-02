74ac642f3d47aeaa5a9223a19e04b529e96e4423
Ulrich Wilfried Motchoffo Kom <motchoffow@gmail.com>
2020-08-02 14:58:45 -0700

    modified the App.js file

--- js/App.js
+++ js/App.js
@@ -12,89 +12,68 @@ var scrore = 0;
 
 //this fuction will ask yes or no question to the user
 function YesAndNoQuestion(){
-
-    // document.write('<h3>answer the following question by yes,y or no,n</h3>');
     alert('nswer the following question by yes,y or no,n');
     var answer =prompt('I\'m under 26 years old');
     answer = answer.toLowerCase();
     var response;
     if (answer === 'yes' || answer === 'y'){
-        // console.log('that\'s correct!');
         response = 'that\'s correct!';
         scrore++;
     }else if( answer === 'no' || answer === 'n'){
-        // console.log('I\'m actually 25 years old :(');
         response = 'I\'m actually 25 years old :(';
     }else{
-        // console.log('you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n');
         response = 'you did not answer that question preoprely please make sure to answer the next question by yes,y or no,n';
     }
-    // document.write('<h3>' + response + '</h3>');
     alert(response);
 
     answer = prompt('I was born in Cameroon');
     answer = answer.toLowerCase();
     if (answer === 'yes' || answer === 'y'){
-        //console.log('that\'s right! in central Africa')
         response='that\'s right! in central Africa';
         scrore++;
     }else if( answer === 'no' || answer === 'n'){
-         //console.log('I actually was :(')
         response='I actually was :( ';
     }else{
-        // console.log('you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n');
         response = 'you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n';
     }
-    // document.write('<h3>' + response + '</h3>');
     alert(response);
 
 
     answer = prompt ('I\'m a single child');
     answer = answer.toLowerCase();
     if (answer === 'yes' || answer === 'y'){
-        //console.log('come on man! I\'m Africain... I have 5 sublings')
         response='come on man! I\'m Africain... I have 5 sublings';
     }else if( answer === 'no' || answer === 'n'){
-         //console.log('yep! you got it right! I have 5 sublings')
         response='yep! you got it right! I have 5 sublings';
         scrore++;
     }else{
-        // console.log('you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n');
         response = 'you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n';
     }
-    // document.write('<h3>' + response + '</h3>');
     alert(response);
 
     answer=prompt('I have a pet');
     answer = answer.toLowerCase();
     if (answer === 'yes' || answer === 'y'){
-        //console.log('you got it! his name is Symba!');
         response='you got it! his name is Symba!';
         scrore++;
     }else if( answer === 'no' || answer === 'n'){
-         //console.log('you wrong!! ;)')
         response='you wrong!! ;)';
     }else{
-        // console.log('you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n');
         response = 'you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n';
     }
-    // document.write('<h3>' + response + '</h3>');
     alert(response);
 
     answer=prompt('I\'m a Christian');
     answer = answer.toLowerCase();
     if (answer === 'yes' || answer === 'y'){
-        //console.log('that\'s correct! I love Jesus :)');
+
         response='that\'s correct! I love Jesus :)';
         scrore++;
     }else if( answer === 'no' || answer === 'n'){
-         //console.log('I am! born and raise ;)')
         response='I am! born and raise ;)';
     }else{
-        // console.log('you did not answer tht question preoprely please make sure to answer the next question by yes,y or no,n');
         response = 'you did not answer that question preoprely but it\' fine you will get the chance to take a look at my web site';
     }
-    // document.write('<h3>' + response + '</h3>');
     alert(response);
 }
 
